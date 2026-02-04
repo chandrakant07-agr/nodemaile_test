fefd177707a78a3293a01a2f2afeb158afd4bda6
Chandrakant Agrawal <agrawal007chandrakant@gmail.com>
2026-02-04 18:11:22 +0530

    added dns

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ app.post('/api/send-email', async (req, res) => {
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
+        },
+        dns: {
+            family: 4
         }
     });
 
